284343f8e085be598fa2e7843b25bd13d64a780b
Alinamoskva090696 <alinamoskva2019@gmail.com>
2025-02-18 22:03:14 +0200

    Added cart total calculation function

---
+++ cart-total.js
@@ -0,0 +1,28 @@
+function calculateCartTotal(cart) {
+    let total = 0;
+
+    for (let item of cart) {
+        total += item.price * item.quantity;
+    }
+
+    if (total > 100) {
+        total *= 0.9;
+    }
+
+    return total;
+}
+
+
+const cart1 = [
+    { name: "Apple", price: 1, quantity: 10 },
+    { name: "Banana", price: 3, quantity: 5 },
+    { name: "Milk", price: 10, quantity: 2 }
+];
+
+const cart2 = [
+    { name: "Sushi", price: 20, quantity: 4 },
+    { name: "Burger Max", price: 30, quantity: 1 }
+];
+
+console.log(calculateCartTotal(cart1));
+console.log(calculateCartTotal(cart2));
